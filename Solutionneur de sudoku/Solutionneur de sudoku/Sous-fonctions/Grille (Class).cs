d2ef16dc66c3514221c143ae0d515ef61b9d00f8
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-09-18 11:12:55 -0400

    Test WebHook

--- Solutionneur de sudoku/Solutionneur de sudoku/Sous-fonctions/Grille (Class).cs
+++ Solutionneur de sudoku/Solutionneur de sudoku/Sous-fonctions/Grille (Class).cs
@@ -9,9 +9,16 @@ namespace Solutionneur_de_sudoku.Sous_fonctions
     public partial class Grille
     {
         internal Case[,] casesDeSudoku = new Case[9, 9];
+        bool caseResolue = true;
+        bool grilleResolue = false;
         internal void valeursEntrees(object sender, EventArgs e)
         {
             CasesInitiales(this);
+
+            while (caseResolue || !grilleResolue)
+            {
+                 
+            }
         }
 
     }
