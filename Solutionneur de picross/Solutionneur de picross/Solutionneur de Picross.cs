0325271389a8599047a187816aea2e12ee9e9c9e
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-09-23 08:36:45 -0400

    Modifications de base pour le nouveau projet; aucun code n'a encore été fait

--- Solutionneur de picross/Solutionneur de picross/Solutionneur de Picross.cs
+++ Solutionneur de picross/Solutionneur de picross/Solutionneur de Picross.cs
@@ -16,5 +16,10 @@ namespace Solutionneur_de_picross
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
