eec94fda3ab5a6054a8c3fc03adb56ac26482e6e
Meuleuh <32850732+Meuleuh@users.noreply.github.com>
2019-10-02 09:17:55 -0400

    Ajout de notes (incomplètes d'ailleurs) sur ce qui va se dérouler dans la phase 3

--- Solutionneur de picross/Solutionneur de picross/Solutionneur de Picross.cs
+++ Solutionneur de picross/Solutionneur de picross/Solutionneur de Picross.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Solutionneur_de_picross
 {
-    public partial class Form1 : Form
+    public partial class Form1 : Form //Phase 1
     {
         //Création des variables
         NumericUpDown horizontalSize = new NumericUpDown();
@@ -62,7 +62,7 @@ namespace Solutionneur_de_picross
             Controls.Add(sizeSetted);
         }
 
-        private void SizeSetted_Click(object sender, EventArgs e)
+        private void SizeSetted_Click(object sender, EventArgs e) //Phase 2
         {
             //On cache les composantes pour entrer en phase 2
             Controls.Remove(horizontalSize);
@@ -125,9 +125,41 @@ namespace Solutionneur_de_picross
             Controls.Add(bg);
             Controls.Add(grilleEntree);
 
-            void GrilleEntree_Click(object sender, EventArgs e)
+            void GrilleEntree_Click(object sender, EventArgs e) //Phase 3
             {
-
+                /*
+                 * Ordre de la résolution (N'est peut-être pas la meilleure / optimale, c'est juste pour la compréhension du code)
+                 * (Certaines fonctions seront dans une boucle do while alors que d'autres ne le seront pas; les fonctions étant
+                 *  hors de cette boucle vont ASSURÉMENT fonctionner qu'UNE SEULE FOIS car en aucun cas, la possibilité de
+                 *  résolution en suivant la dite règle va se présenter plus tard dans l'exécution si cette possibilité n'était pas
+                 *  présente dès le début)
+                 * 1- Règle de la ligne pleine: Si une des valeurs est égale à la taille de sa rangée/colonne, remplir
+                 *    automatiquement cette dernière
+                 * 2- Règle de la ligne vide: Si une rangée/colonne a comme valeur seulement un '0', mettre les marqueurs
+                 *    d'espaces sur toute la rangée
+                 * 3- Règle de la moitié: Si une des valeurs est supérieure ou égale à la moitié de la taille de la rangée/colonne 
+                 *    arrondi vers le haut / + 1 (donc à partir de 8 pour une taille de 14 ou 15), ajouter des cases au centre selon
+                 *    la valeur (à chaque +1 par rapport à la moitié, ajouter une case de chaque côté en partant du centre
+                 * 4- Règle du cumul: Lorsque l'addition des valeurs entrées pour la rangée/colonne ainsi que les espaces
+                 *    nécessitent toute la rangée, elle peut être automatiquement résolue sans se poser de questions
+                 * 5- Règle des cases obligatoires: Lorsque l'addition des valeurs entrées ainsi que les espaces laisse un nombre
+                 *    de cases "amovibles" inférieure à une ou des valeurs entrées, il est possible de situer certaines de ces
+                 *    cases en vérifiant quelles cases sont valides dans tous les cas (Tester si toutes les valeurs sont à une
+                 *    extrémité puis à l'autre et voir quelles cases sont présentes dans les deux cas ?)
+                 * 6- Règle des murs: Lorsqu'une zone rencontre un mur (par exemple, lorsqu'une zone de 2 commence collée contre
+                 *    un espace), elle va obligatoirement dans une direction (vu que l'autre a été bouchée) donc on peut rentrer
+                 *    les cases directement dans la grille
+                 * 7- Règle de la portée individuelle: Quand il n'y a qu'une valeur dans la rangée/colonne et qu'au moins une case
+                 *    a été trouvée pour cette rangée/colonne, on peut déterminer quelles cases seront obligatoirement des espaces
+                 *    en prenant en compte les cases impossibles à atteindre dans la portée de la valeur (par exemple, si une
+                 *    valeur 3 est située en plein milieu de sa rangée en position 4, il est impossible que les cases 1 et 7 soient
+                 *    prises pour le 3 car elles sont hors de portées)
+                 * 8- Règle des entrées trop grandes: Lorsque l'on a un espace trop petit pour une valeur (par exemple, un espace
+                 *    de 1 pour une série de 2) et que cette valeur est obligatoirement à cette endroit dans la rangée/colonne
+                 *    (première ou dernière valeur en sont de simples exemples), on peut automatiquement placer des espaces dans
+                 *    ces zones trop petites
+                 * 9- 
+                 */
             }
         }
 
